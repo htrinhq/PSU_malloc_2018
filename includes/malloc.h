6a94c6c6938e7be911fe8129ee997ee6749a48f2
Hugo TRINH <hugo.trinh-quy@epitech.eu>
2019-01-22 14:52:01 +0100

    add new function and add some macros

--- includes/malloc.h
+++ includes/malloc.h
@@ -11,11 +11,15 @@
 #include <unistd.h>
 #include <stdint.h>
 
-typedef struct s_block *t_block;
-
 typedef uint8_t bool_t;
+
 #define true 1
 #define false 0
+
+#define ALIGN(x)  (((((x) - 1) >> 2) << 2) + 4)
+#define BLOCK_SIZE sizeof(struct s_block)
+
+typedef struct s_block *t_block;
 
 struct s_block {
 	size_t size;
--- sources/malloc.c
+++ sources/malloc.c
@@ -7,7 +7,18 @@
 
 #include "malloc.h"
 
+t_block new(size_t size)
+{
+	t_block new = sbrk(0);
+if (sbrk(BLOCK_SIZE + size) == (void *) -1)
+		return NULL;
+	new->size = size;
+	new->free = false;
+	new->next = NULL;
+	return new;
+}
+
 void *malloc(size_t size)
 {
-	
+
 }
